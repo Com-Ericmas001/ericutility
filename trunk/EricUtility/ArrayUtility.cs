5cd36fc9a8b7c7057fcc3442f91bdebeefdc7141
ericmas001 <ericmas001@7f244a26-2feb-828a-003a-c2b9a63f6a55>
2010-03-29 01:46:50 +0000

    cool stuff

---
+++ trunk/EricUtility/ArrayUtility.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EricUtility
+{
+    public class ArrayUtility
+    {
+        /// <summary>
+        /// Choisis un élément dans la liste parmis tous les minimums
+        /// </summary>
+        /// <typeparam name="T">Le type d'objet du Array</typeparam>
+        /// <param name="values">L'array</param>
+        /// <returns>La position de la valeur choisie dans le array</returns>
+        public static int GetRandomMin<T>(T[] values) where T : IComparable<T>
+        {
+            T min = values[0];
+            List<int> possibles = new List<int>() { 0 };
+            for (int i = 1; i < 16; ++i)
+            {
+                if (values[i].CompareTo(min) < 0)
+                {
+                    min = values[i];
+                    possibles.Clear();
+                }
+
+                if (values[i].CompareTo(min) == 0)
+                    possibles.Add(i);
+            }
+            return possibles[Hasard.RandomWithMax(possibles.Count - 1)];
+        }
+        /// <summary>
+        /// Choisis un élément dans la liste parmis tous les maximums
+        /// </summary>
+        /// <typeparam name="T">Le type d'objet du Array</typeparam>
+        /// <param name="values">L'array</param>
+        /// <returns>La position de la valeur choisie dans le array</returns>
+        public static int GetRandomMax<T>(T[] values) where T : IComparable<T>
+        {
+            T max = values[0];
+            List<int> possibles = new List<int>() { 0 };
+            for (int i = 1; i < 16; ++i)
+            {
+                if (values[i].CompareTo(max) > 0)
+                {
+                    max = values[i];
+                    possibles.Clear();
+                }
+
+                if (values[i].CompareTo(max) == 0)
+                    possibles.Add(i);
+            }
+            return possibles[Hasard.RandomWithMax(possibles.Count - 1)];
+        }
+    }
+}
---
+++ trunk/EricUtility/BitUtility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EricUtility
+{
+    public class BitUtility
+    {
+        /// <summary>
+        /// Compte le nombre de bits allumés
+        /// </summary>
+        /// <param name="n">Le nombre</param>
+        /// <returns>Le nombre de bits</returns>
+        public static int CountBits(int n)
+        {
+            int count;
+            for (count = 0; n != 0; count++)
+                n &= (n - 1);
+            return count;
+        }
+    }
+}
