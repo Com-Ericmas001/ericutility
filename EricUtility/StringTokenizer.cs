ef9b79e7b36bac85511d637fd89cd2951ca0b94b
Éric Massé <Ericmas001@users.noreply.github.com>
2010-04-09 20:00:14 +0000

    StringTokenizer

---
+++ EricUtility/StringTokenizer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EricUtility
+{
+    public class StringTokenizer
+    {
+        private string m_TotalStr;
+        private string[] m_SplittedStr;
+        private int m_Index;
+        private string m_Delimitter;
+
+        public StringTokenizer(string str, string delimitter)
+        {
+            m_TotalStr = str;
+            m_Delimitter = delimitter;
+            m_SplittedStr = str.Split(delimitter);
+            m_Index = -1;
+        }
+
+        public void Reset()
+        {
+            m_Index = -1;
+        }
+
+        public bool HasMoreTokens()
+        {
+            return CountTokens() > m_Index;
+        }
+
+        public int CountTokens()
+        {
+            return m_SplittedStr.Length;
+        }
+
+        public string nextToken()
+        {
+            m_Index++;
+            if (HasMoreTokens())
+                return m_SplittedStr[m_Index];
+            else
+                return "";
+        }
+    }
+}
