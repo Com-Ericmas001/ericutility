3d107a520287d053f0bbbdca739d60864823445f
Éric Massé <Ericmas001@users.noreply.github.com>
2010-09-09 18:31:43 +0000

    Networking: Added Queue Communication

---
+++ EricUtilityNetworking/Commands/CommandQueueCommunicator.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System;
+
+namespace EricUtility.Networking.Commands
+{
+    public abstract class CommandQueueCommunicator<T> : QueueCommunicator where T : CommandObserver, new()
+    {
+        protected T m_CommandObserver = new T();
+
+        public CommandQueueCommunicator(TcpClient socket)
+        {
+            InitializeCommandObserver();
+            base.ReceivedSomething += new EventHandler<KeyEventArgs<string>>(CommandQueueCommunicator_ReceivedSomething);
+        }
+
+        void CommandQueueCommunicator_ReceivedSomething(object sender, KeyEventArgs<string> e)
+        {
+            m_CommandObserver.messageReceived(e.Key);
+        }
+
+        protected abstract void InitializeCommandObserver();
+
+        public virtual void Send(AbstractCommand command)
+        {
+            base.Send(command.Encode());
+        }
+    }
+}
---
+++ EricUtilityNetworking/QueueCommunicator.cs
@@ -0,0 +1,49 @@
+﻿using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System;
+using EricUtility.Collections;
+
+namespace EricUtility.Networking
+{
+    public class QueueCommunicator
+    {
+        protected bool m_IsConnected;
+        protected BlockingQueue<String> m_Incoming = new BlockingQueue<String>();
+        protected BlockingQueue<String> m_Outcoming = new BlockingQueue<String>();
+        public event EventHandler<KeyEventArgs<string>> ReceivedSomething = delegate { };
+        public event EventHandler<KeyEventArgs<string>> SendedSomething = delegate { };
+        public QueueCommunicator()
+        {
+            m_IsConnected = false;
+        }
+
+        public virtual bool IsConnected
+        {
+            get { return m_IsConnected; }
+            set { m_IsConnected = value; } 
+        }
+
+        protected string Receive()
+        {
+            return m_Incoming.Dequeue();
+        }
+
+        public void Send(string line)
+        {
+            m_Outcoming.Enqueue(line);
+        }
+        protected virtual void Run()
+        {
+            while (IsConnected)
+            {
+                Receive();
+            }
+        }
+        public void Start()
+        {
+            new Thread(new ThreadStart(Run)).Start();
+        }
+
+    }
+}
