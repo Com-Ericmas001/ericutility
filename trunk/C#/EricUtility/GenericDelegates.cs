d68b8d7e83965adf5ce4a1920fa21c9bfad1e621
ericmas001 <ericmas001@7f244a26-2feb-828a-003a-c2b9a63f6a55>
2010-09-10 20:03:05 +0000

    Populate EricUtility

---
+++ trunk/C#/EricUtility/GenericDelegates.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EricUtility
+{
+    public delegate void EmptyHandler();
+    public delegate void BooleanHandler(bool value);
+    public delegate void StringHandler(string value);
+}
