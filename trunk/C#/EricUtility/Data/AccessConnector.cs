766487f822b3cbb687ecbbab423c8eaccf8c9a8b
ericmas001 <ericmas001@7f244a26-2feb-828a-003a-c2b9a63f6a55>
2010-09-10 20:08:19 +0000

    C#: EricUtility.Data created !

---
+++ trunk/C#/EricUtility/Data/AccessConnector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.OleDb;
+
+namespace EricUtility.Data
+{
+    public class AccessConnector : AbstractConnector
+    {
+        public AccessConnector(string path)
+            : base(path)
+        {
+        }
+        protected override void Connect()
+        {
+            string Provider = "Microsoft.ACE.OLEDB.12.0";
+            string connectionString = String.Format("Provider={0};Data Source={1}", Provider, Path);
+            Connexion = new OleDbConnection(connectionString);
+            Connexion.Open();
+        }
+    }
+}
