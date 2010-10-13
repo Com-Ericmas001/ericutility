13a9a320f565b2d7792779a44b06a172fc5332c3
ericmas001 <ericmas001@7f244a26-2feb-828a-003a-c2b9a63f6a55>
2010-10-13 21:23:04 +0000

    CustomizedTabPage takes a Usercontrol as content docked "fill"

---
+++ trunk/C#/EricUtilityWinForms/TabControl/CustomizedTabPage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EricUtility.Windows.Forms
+{
+    public partial class CustomizedTabPage : TabPage
+    {
+        public CustomizedTabPage(TabPageContent content) : 
+            base()
+        {
+            content.Dock = DockStyle.Fill;
+            Controls.Add(content);
+        }
+    }
+}
---
+++ trunk/C#/EricUtilityWinForms/TabControl/TabPageContent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EricUtility.Windows.Forms
+{
+    public partial class TabPageContent : UserControl
+    {
+        public TabPageContent()
+        {
+            InitializeComponent();
+        }
+    }
+}
