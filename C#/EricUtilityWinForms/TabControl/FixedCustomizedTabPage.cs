e14332c63b946cb49f0b45c484c444a4bbdd4d8c
Éric Massé <Ericmas001@users.noreply.github.com>
2010-10-13 21:23:04 +0000

    CustomizedTabPage takes a Usercontrol as content docked "fill"

---
+++ C#/EricUtilityWinForms/TabControl/FixedCustomizedTabPage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EricUtility.Windows.Forms
+{
+    public partial class FixedCustomizedTabPage : CustomizedTabPage, INonCloseableTabPage
+    {
+
+        public FixedCustomizedTabPage(TabPageContent content) : base(content)
+        {
+        }
+    }
+}
