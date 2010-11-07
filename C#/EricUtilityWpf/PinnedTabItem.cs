f3e16e0bf46f58474b34dea11be3cb4964b307b9
Éric Massé <Ericmas001@users.noreply.github.com>
2010-11-07 02:45:26 +0000

    PinnedTabWPF

---
+++ C#/EricUtilityWpf/PinnedTabItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+
+namespace EricUtility.Windows
+{
+    public class PinnedTabItem : TabItem
+    {
+    }
+}
