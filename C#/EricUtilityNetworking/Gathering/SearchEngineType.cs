6a5fa3f12534c7a9fc4416b13a1c0d31e43c2ce4
Éric Massé <Ericmas001@users.noreply.github.com>
2010-11-27 02:18:43 +0000

    Search with google API

---
+++ C#/EricUtilityNetworking/Gathering/SearchEngineType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EricUtility.Networking.Gathering
+{
+    public enum SearchEngineType
+    {
+        Unknown,
+        Google,
+        Bing
+    }
+}
---
+++ C#/EricUtilityNetworking/Gathering/SearchResultEntry.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EricUtility.Networking.Gathering
+{
+    public class SearchResultEntry
+    {
+        private string m_Url;
+        private string m_Title;
+        private string m_Content;
+        private SearchEngineType m_Engine;
+
+        public string Url
+        {
+            get { return m_Url; }
+            set { m_Url = value; }
+        }
+
+        public string Title
+        {
+            get { return m_Title; }
+            set { m_Title = value; }
+        }
+
+        public string Content
+        {
+            get { return m_Content; }
+            set { m_Content = value; }
+        }
+
+        public SearchEngineType Engine
+        {
+            get { return m_Engine; }
+            set { m_Engine = value; }
+        }
+
+        public SearchResultEntry(string url, string title, string content, SearchEngineType engine)
+        {
+          m_Url = url;
+          m_Title = title;
+          m_Content = content;
+          m_Engine = engine;
+        }
+    }
+}
