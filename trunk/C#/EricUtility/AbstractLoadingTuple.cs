35398f2392a4ed1fcd82fa2c813b55da37ef4735
ericmas001 <ericmas001@7f244a26-2feb-828a-003a-c2b9a63f6a55>
2010-12-17 19:41:25 +0000

    AbstractLoadingTuple

---
+++ trunk/C#/EricUtility/AbstractLoadingTuple.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EricUtility;
+
+namespace EricUtility
+{
+    public abstract class AbstractLoadingTuple
+    {
+        protected bool m_Loaded = false;
+        private bool m_Loading = false;
+        public bool Loaded { get { return m_Loaded; } }
+
+        protected abstract void OnLoad();
+
+        public void ForceLoad()
+        {
+            NeedToReload();
+            LoadIfNeeded();
+        }
+
+        public void NeedToReload()
+        {
+            if( !m_Loading )
+                m_Loaded = false;
+        }
+
+        public void LoadIfNeeded()
+        {
+            if (!m_Loaded && !m_Loading)
+            {
+                m_Loading = true;
+                OnLoad();
+                m_Loaded = true;
+                m_Loading = false;
+            }
+        }
+    }
+}
