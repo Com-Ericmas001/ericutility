adf22fa8eddc4374220779519376077793e0476f
Éric Massé <Ericmas001@users.noreply.github.com>
2011-06-20 22:22:39 +0000

    IntHandler

--- C#/EricUtility/GenericDelegates.cs
+++ C#/EricUtility/GenericDelegates.cs
@@ -7,4 +7,5 @@ namespace EricUtility
     public delegate void EmptyHandler();
     public delegate void BooleanHandler(bool value);
     public delegate void StringHandler(string value);
+    public delegate void IntHandler(int value);
 }
