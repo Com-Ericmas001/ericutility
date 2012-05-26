ad0f348c78de6440113543b557eb72e29da5fd4b
Éric Massé <Ericmas001@users.noreply.github.com>
2012-05-26 12:41:10 +0000

    Ajout DateTimeWithZone

---
+++ C#/EricUtility/DateTimeWithZone.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EricUtility2011
+{
+    public class DateTimeWithZone
+    {
+        private readonly DateTime utcDateTime;
+        private readonly TimeZoneInfo timeZone;
+
+        public DateTimeWithZone(DateTime dateTime, TimeZoneInfo timeZone)
+        {
+            utcDateTime = TimeZoneInfo.ConvertTimeToUtc(dateTime, timeZone);
+            this.timeZone = timeZone;
+        }
+
+        public DateTime UniversalTime { get { return utcDateTime; } }
+
+        public TimeZoneInfo TimeZone { get { return timeZone; } }
+
+        public DateTime LocalTime
+        {
+            get
+            {
+                return TimeZoneInfo.ConvertTime(utcDateTime, timeZone);
+            }
+        }
+    }
+}
