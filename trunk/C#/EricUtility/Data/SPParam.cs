46c4b03982ee4544eb3a2e530ba07c845d5003a3
ericmas001 <ericmas001@7f244a26-2feb-828a-003a-c2b9a63f6a55>
2013-03-17 14:41:50 +0000

    All New "Stored Procedure" notion in SqlServerConnector

---
+++ trunk/C#/EricUtility/Data/SPParam.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace EricUtility2011.Data
+{
+    public enum ParamDir
+    {
+        Input,
+        Output
+    }
+    public class SPParam
+    {
+        private SqlParameter m_Parm;
+
+        public SqlParameter Parm { get { return m_Parm; } }
+
+        public SPParam(SqlParameter parm)
+            : this(parm, ParamDir.Input, null)
+        {
+
+        }
+        public SPParam(SqlParameter parm, ParamDir dir)
+            : this(parm, dir, null)
+        {
+
+        }
+        public SPParam(SqlParameter parm, object val)
+            : this(parm, ParamDir.Input, val)
+        {
+
+        }
+        public SPParam(SqlParameter parm, ParamDir dir, object val)
+        {
+            m_Parm = parm;
+            if (dir == ParamDir.Input)
+                m_Parm.Direction = System.Data.ParameterDirection.Input;
+            else
+                m_Parm.Direction = System.Data.ParameterDirection.Output;
+            m_Parm.Value = val;
+        }
+    }
+}
