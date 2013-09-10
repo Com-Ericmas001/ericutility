22849e5abb4a8a11ab51e1fd27375bfbcb497dea
ericmas001 <ericmas001@7f244a26-2feb-828a-003a-c2b9a63f6a55>
2013-09-10 20:37:58 +0000

    + Downloader

---
+++ trunk/C#/EricUtilityWinForms/Downloader/ClipboardHelper.cs
@@ -0,0 +1,32 @@
+using EricUtilityNetworking.Downloader;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EricUtility.Windows.Forms.Downloader
+{
+    public static class ClipboardHelper
+    {
+        public static string GetURLOnClipboard()
+        {
+            string url = string.Empty;
+
+            if (Clipboard.ContainsText())
+            {
+                string tempUrl = Clipboard.GetText();
+
+                if (ResourceLocation.IsURL(tempUrl))
+                {
+                    url = tempUrl;
+                }
+                else
+                {
+                    tempUrl = null;
+                }
+            }
+
+            return url;
+        }
+    }
+}
