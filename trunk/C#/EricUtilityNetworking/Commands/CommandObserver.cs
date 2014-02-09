3ae44edc24ebc96b6c0f91c6307728f0e71efce6
ericmas001 <ericmas001@7f244a26-2feb-828a-003a-c2b9a63f6a55>
2014-02-09 18:18:36 +0000

    Using reflection to raise event! Magic!

--- trunk/C#/EricUtilityNetworking/Commands/CommandObserver.cs
+++ trunk/C#/EricUtilityNetworking/Commands/CommandObserver.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 namespace EricUtility.Networking.Commands
 {
@@ -6,7 +7,39 @@ namespace EricUtility.Networking.Commands
     {
         public event EventHandler<StringEventArgs> CommandReceived = delegate { };
 
-        protected abstract void receiveSomething(string line);
+        protected abstract char Delimitter { get; }
+
+        /// <summary>
+        /// Méthode éxécutée lorsqu'on recoit une commande
+        /// </summary>
+        /// <param name="line">ligne de commande</param>
+        /// <remarks>La méthode s'occupe de faire un RaiseEvent pour chaque evenemement EventHandler(Of CommandEventArgs(Of T)) où T.COMMAND_NAME est égal au premier token de la ligne reçu</remarks>
+        protected virtual void receiveSomething(string line)
+        {
+
+            StringTokenizer token = new StringTokenizer(line, Delimitter);
+            string commandName = token.NextToken();
+            foreach (EventInfo e in this.GetType().GetEvents())
+            {
+                if (e.EventHandlerType.Name == typeof(EventHandler<>).Name)
+                {
+                    Type eventType = e.EventHandlerType.GenericTypeArguments[0];
+                    if (eventType.Name == typeof(CommandEventArgs<>).Name)
+                    {
+                        Type commType = eventType.GenericTypeArguments[0];
+                        if (commandName == (string)commType.GetField("COMMAND_NAME").GetValue(null))
+                        {
+                            object command = Activator.CreateInstance(commType, token);
+                            object commandEventArgs = Activator.CreateInstance(eventType, command);
+                            MulticastDelegate del = (MulticastDelegate)this.GetType().GetField(e.Name, BindingFlags.Instance | BindingFlags.NonPublic).GetValue(this);
+                            if (del != null)
+                                foreach (Delegate hdl in del.GetInvocationList())
+                                    hdl.Method.Invoke(hdl.Target, new object[] { this, commandEventArgs });
+                        }
+                    }
+                }
+            }
+        }
 
         public virtual void messageReceived(string line)
         {
