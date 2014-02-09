8f48584d27a6112540e1280a17b338b5d172ea2a
ericmas001 <ericmas001@7f244a26-2feb-828a-003a-c2b9a63f6a55>
2014-02-09 20:23:37 +0000

    Simplify the use of COMMAND_NAME

--- trunk/C#/EricUtilityNetworking/Commands/DisconnectCommand.cs
+++ trunk/C#/EricUtilityNetworking/Commands/DisconnectCommand.cs
@@ -4,11 +4,6 @@ namespace EricUtility.Networking.Commands
 {
     public class DisconnectCommand : AbstractCommand
     {
-        protected override string CommandName
-        {
-            get { return DisconnectCommand.COMMAND_NAME; }
-        }
-
         public static string COMMAND_NAME = "DISCONNECT";
 
         public DisconnectCommand(StringTokenizer argsToken)
