0c969327b2f7ca046dadcdee78e384f56a672f47
ericmas001 <ericmas001@7f244a26-2feb-828a-003a-c2b9a63f6a55>
2014-02-23 02:44:56 +0000

    Creating StepSplashForm

---
+++ trunk/C#/EricUtilityWinForms/StepSplashForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EricUtility.Windows.Forms
+{
+    public class StepSplashForm : Form
+    {
+        protected delegate bool BoolEmptyHandler();
+        protected Tuple<BoolEmptyHandler, StatePictureBox>[] m_Steps;
+
+        protected bool ExecuteSteps()
+        {
+            bool ok = true;
+            foreach (Tuple<BoolEmptyHandler, StatePictureBox> step in m_Steps)
+            {
+                if (ok)
+                {
+                    step.Item2.Etat = StatePictureBoxStates.Waiting;
+                    if (step.Item1.Invoke())
+                        step.Item2.Etat = StatePictureBoxStates.Ok;
+                    else
+                    {
+                        step.Item2.Etat = StatePictureBoxStates.Bad;
+                        ok = false;
+                    }
+                }
+            }
+            return ok;
+        }
+    }
+}
