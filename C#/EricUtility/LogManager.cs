261fc8747dd1d6445f24d3ca59487dc16ae56c62
Éric Massé <Ericmas001@users.noreply.github.com>
2014-02-23 01:45:55 +0000

    LogManager new LogInConsole function

--- C#/EricUtility/LogManager.cs
+++ C#/EricUtility/LogManager.cs
@@ -70,6 +70,72 @@ namespace EricUtility
             if (MessageLogged != null)
                 MessageLogged(String.Format(from, args), String.Format(format, args), level);
         }
+
+        public static void LogInConsole(string from, string message, int level, LogLevel minLevelToLog)
+        {
+            ConsoleColor fc = Console.ForegroundColor;
+            ConsoleColor bc = Console.BackgroundColor;
+
+
+            //Errors
+            if (level >= (int)LogLevel.ErrorHigh)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.White;
+                message = "ERROR: " + message;
+            }
+            if (level >= (int)LogLevel.Error)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                message = "ERROR: " + message;
+            }
+            else if (level >= (int)LogLevel.ErrorLow)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+            }
+
+
+            //Warnings
+            else if (level >= (int)LogLevel.Warning)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                message = "WARNING: " + message;
+            }
+            else if (level >= (int)LogLevel.WarningLow)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+            }
+
+
+            //Messages
+            else if (level >= (int)LogLevel.MessageVeryHigh)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                message = "IMPORTANT: " + message;
+            }
+            else if (level >= (int)LogLevel.Message)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if (level >= (int)LogLevel.MessageLow)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                message = "DEBUG: " + message;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                message = "DEBUG: " + message;
+            }
+
+
+            //Let's Log!
+            if (level >= (int)minLevelToLog)
+                Console.WriteLine(message);
+
+            Console.ForegroundColor = fc;
+            Console.BackgroundColor = bc;
+        }
     }
 }
 
