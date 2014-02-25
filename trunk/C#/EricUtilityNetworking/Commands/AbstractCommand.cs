43d1c1a74b98b8159386b9af54111726f849084b
ericmas001 <ericmas001@7f244a26-2feb-828a-003a-c2b9a63f6a55>
2014-02-25 12:51:58 +0000

    Creating JSon Command Structure

--- trunk/C#/EricUtilityNetworking/Commands/AbstractCommand.cs
+++ trunk/C#/EricUtilityNetworking/Commands/AbstractCommand.cs
@@ -7,27 +7,9 @@ namespace EricUtility.Networking.Commands
     {
         public static string CommandNameField { get { return "COMMAND_NAME"; } }
 
-        public static char Delimitter { get { return ';'; } }
+        public string CommandName { get { return (string)this.GetType().GetField(CommandNameField).GetValue(null); } }
 
-        protected string CommandName { get { return (string)this.GetType().GetField(CommandNameField).GetValue(null); } }
-
-        public virtual void Encode(StringBuilder sb)
-        {
-        }
-
-        protected virtual void Append<T>(StringBuilder sb, T thing)
-        {
-            sb.Append(thing);
-            sb.Append(AbstractCommand.Delimitter);
-        }
-
-        public string Encode()
-        {
-            StringBuilder sb = new StringBuilder();
-            Append(sb, CommandName);
-            Encode(sb);
-            return sb.ToString();
-        }
+        public abstract string Encode();
 
         public AbstractCommand()
         {
