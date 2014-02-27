355765c629453fcd0ad8aa2ab72582e077c67921
Éric Massé <Ericmas001@users.noreply.github.com>
2014-02-27 23:05:37 +0000

    Better Json Command Support

--- C#/EricUtilityNetworking/Commands/AbstractCommand.cs
+++ C#/EricUtilityNetworking/Commands/AbstractCommand.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using System.Text;
 
 namespace EricUtility.Networking.Commands
@@ -7,13 +8,13 @@ namespace EricUtility.Networking.Commands
     {
         public static string CommandNameField { get { return "COMMAND_NAME"; } }
 
-        public string CommandName { get { return (string)this.GetType().GetField(CommandNameField).GetValue(null); } }
+        public string CommandName { get { return (string)this.GetType().GetField(CommandNameField, (BindingFlags.Static | BindingFlags.FlattenHierarchy | BindingFlags.Public)).GetValue(null); } }
 
         public abstract string Encode();
 
         public AbstractCommand()
         {
-            if (this.GetType().GetField(CommandNameField) == null)
+            if (this.GetType().GetField(CommandNameField, (BindingFlags.Static | BindingFlags.FlattenHierarchy | BindingFlags.Public)) == null)
                 throw new Exception("You need a public static field named '" + CommandNameField + "' that gives the name of the command!");
         }
     }
