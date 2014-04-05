676374e38c1555714dfc7a66c137720430b3aff4
Éric Massé <Ericmas001@users.noreply.github.com>
2014-04-05 18:57:57 +0000

    Updating Json Parameters

--- C#/EricUtilityNetworking/Commands/Json/AbstractJsonCommand.cs
+++ C#/EricUtilityNetworking/Commands/Json/AbstractJsonCommand.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Runtime.Serialization.Formatters;
 using System.Text;
 
 namespace EricUtility.Networking.Commands
@@ -8,7 +9,7 @@ namespace EricUtility.Networking.Commands
     {
         public override string Encode()
         {
-            return JsonConvert.SerializeObject(this, new JsonSerializerSettings() { TypeNameHandling = TypeNameHandling.All });
+            return JsonConvert.SerializeObject(this, new JsonSerializerSettings() { TypeNameHandling = TypeNameHandling.All, TypeNameAssemblyFormat = FormatterAssemblyStyle.Simple });
         }
     }
 }
