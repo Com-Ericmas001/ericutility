ea641998fbe03b6e737f48d531e12453ed88410a
ericmas001 <ericmas001@7f244a26-2feb-828a-003a-c2b9a63f6a55>
2014-04-06 13:54:46 +0000

    Com.Ericmas001.Games

---
+++ trunk/C#/Com.Ericmas001/Com.Ericmas001.Games/GameCardSpecial.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Ericmas001.Games
+{
+    public enum GameCardSpecial
+    {
+        None,
+        Null,
+        Hidden,
+        JokerColor,
+        JokerDark,
+    }
+}
---
+++ trunk/C#/Com.Ericmas001/Com.Ericmas001.Games/GameCardValue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Ericmas001.Games
+{
+    public enum GameCardValue
+    {
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace
+    }
+}
