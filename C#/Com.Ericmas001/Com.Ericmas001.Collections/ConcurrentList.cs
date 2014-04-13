17c6e7f53739f6a9c7801083a332c1fc0fff765d
Éric Massé <Ericmas001@users.noreply.github.com>
2014-04-13 00:19:35 +0000

    Using ConcurrentList for ThreadSafe List

---
+++ C#/Com.Ericmas001/Com.Ericmas001.Collections/ConcurrentList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Ericmas001.Collections
+{
+    public class ConcurrentList<T> : ConcurrentDictionary<T, T>
+    {
+        public ConcurrentList()
+            : base()
+        {
+
+        }
+        public ConcurrentList(ConcurrentList<T> list)
+            : base()
+        {
+        }
+
+        public List<T> ToList()
+        {
+            return Keys.ToList();
+        }
+
+        public void Add(T item)
+        {
+            base.AddOrUpdate(item, item, delegate { return item; });
+        }
+
+        public bool Contains(T item)
+        {
+            return base.ContainsKey(item);
+        }
+
+        public void Remove(T item)
+        {
+            T removed;
+            base.TryRemove(item, out removed);
+        }
+    }
+}
