58c373be3345bc94e26e48adb70be95daec961e5
ericmas001 <ericmas001@7f244a26-2feb-828a-003a-c2b9a63f6a55>
2014-04-18 18:36:14 +0000

    IOption & FactoryOption now in the Com.Ericmas001.Util package + OptionJsonConverter in Com.Ericmas001.Net.JSON

---
+++ trunk/C#/Com.Ericmas001/Com.Ericmas001.Util/Options/FactoryOption.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Ericmas001.Util.Options
+{
+    public static class FactoryOption<TOption, TEnum> 
+        where TOption : IOption<TEnum>
+    {
+        public static TOption GenerateOption(int enumValue)
+        {
+            if (!typeof(TEnum).IsEnum)
+            {
+                throw new ArgumentException("TEnum must be an enumerated type");
+            }
+
+            foreach (Assembly a in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                foreach (Type t in a.GetTypes().Where(t => t != typeof(TOption) && typeof(TOption).IsAssignableFrom(t)))
+                {
+                    TOption instance = (TOption)Activator.CreateInstance(t);
+                    if (Convert.ToInt32(instance.OptionType) == enumValue)
+                        return instance;
+                }
+            }
+            return default(TOption);
+        }
+    }
+}
