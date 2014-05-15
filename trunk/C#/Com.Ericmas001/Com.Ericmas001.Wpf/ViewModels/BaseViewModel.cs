6547becebc35e37496f1dd9322eacccfd5762d71
ericmas001 <ericmas001@7f244a26-2feb-828a-003a-c2b9a63f6a55>
2014-05-15 01:38:10 +0000

    BgTaskManager, BaseViewModel & WpfWindowManager

---
+++ trunk/C#/Com.Ericmas001/Com.Ericmas001.Wpf/ViewModels/BaseViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Ericmas001.Wpf.ViewModels
+{
+    public class BaseViewModel
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+        public event EventHandler OnRequestClose = delegate { };
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ trunk/C#/Com.Ericmas001/Com.Ericmas001.Wpf/WpfWindowManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Com.Ericmas001.Wpf.ViewModels;
+
+namespace Com.Ericmas001.Wpf
+{
+    public static class WpfWindowManager
+    {
+        public static T Show<T>(this Window window) where T : BaseViewModel
+        {
+            T vm = window.DataContext as T;
+            vm.OnRequestClose += (s, e) => window.Close();
+            window.Show();
+            return vm;
+        }
+        public static T ShowDialog<T>(this Window window) where T : BaseViewModel
+        {
+            T vm = window.DataContext as T;
+            vm.OnRequestClose += (s, e) => window.Close();
+            window.ShowDialog();
+            return vm;
+        }
+    }
+}
