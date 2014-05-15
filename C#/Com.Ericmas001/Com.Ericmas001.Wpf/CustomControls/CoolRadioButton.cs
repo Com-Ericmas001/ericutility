d0af6f7e18c081c6d1b2c0d96714fc6e209a107c
Éric Massé <Ericmas001@users.noreply.github.com>
2014-05-15 01:25:49 +0000

    Merging stuff from ia.gui

---
+++ C#/Com.Ericmas001/Com.Ericmas001.Wpf/CustomControls/CoolRadioButton.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace Com.Ericmas001.Wpf.CustomControls
+{
+    public class CoolRadioButton : RadioButton
+    {
+        public static readonly DependencyProperty ImageSourceProperty =
+           DependencyProperty.Register("ImageSource",
+                                       typeof(BitmapImage),
+                                       typeof(CoolRadioButton),
+                                       new FrameworkPropertyMetadata(null));
+
+        public static readonly DependencyProperty ImageSizeProperty =
+           DependencyProperty.Register("ImageSize",
+                                       typeof(int),
+                                       typeof(CoolRadioButton),
+                                       new FrameworkPropertyMetadata(0));
+
+        public BitmapImage ImageSource
+        {
+            get { return (BitmapImage)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+
+        public int ImageSize
+        {
+            get { return (int)GetValue(ImageSizeProperty); }
+            set { SetValue(ImageSizeProperty, value); }
+        }
+
+        static CoolRadioButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CoolRadioButton), new FrameworkPropertyMetadata(typeof(CoolRadioButton)));
+        }
+    }
+
+}
