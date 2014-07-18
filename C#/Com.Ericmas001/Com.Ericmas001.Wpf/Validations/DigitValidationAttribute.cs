e18e4e86942808c129c5f42f4f508727a35f420a
Éric Massé <Ericmas001@users.noreply.github.com>
2014-07-18 23:35:38 +0000

    More for WPF lib

---
+++ C#/Com.Ericmas001/Com.Ericmas001.Wpf/Validations/DigitValidationAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Com.Ericmas001.Wpf.Validations
+{
+    public class DigitValidationAttribute : SimpleValidationAttribute
+    {
+        public override string Validate(string value)
+        {
+            if (String.IsNullOrEmpty(value))
+                return null;
+
+            return !value.All(Char.IsDigit) ? String.Format("Seul des chiffres sont acceptés!") : null;
+        }
+    }
+}
---
+++ C#/Com.Ericmas001/Com.Ericmas001.Wpf/Validations/LetterOrDigitValidationAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Com.Ericmas001.Wpf.Validations
+{
+    public class LetterOrDigitValidationAttribute : SimpleValidationAttribute
+    {
+        public override string Validate(string value)
+        {
+            if (String.IsNullOrEmpty(value))
+                return null;
+            return !value.All(Char.IsLetterOrDigit) ? String.Format("Seul des lettres ou des chiffres sont acceptés!") : null;
+        }
+    }
+}
