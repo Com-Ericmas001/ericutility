0c3e097299f82db405082bf52f31c1d47e5705a2
Éric Massé <Ericmas001@users.noreply.github.com>
2014-10-07 19:32:30 +0000

    DLLs Version 4.0.0

--- C#/Com.Ericmas001/Com.Ericmas001.Games.Windows.Forms/Properties/AssemblyInfo.cs
+++ C#/Com.Ericmas001/Com.Ericmas001.Games.Windows.Forms/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Com.Ericmas001.Games.Windows.Forms")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ericmas001@gmail.com")]
 [assembly: AssemblyProduct("Com.Ericmas001.Games.Windows.Forms")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
--- C#/Com.Ericmas001/Com.Ericmas001.Wpf/Properties/AssemblyInfo.cs
+++ C#/Com.Ericmas001/Com.Ericmas001.Wpf/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyTitle("Com.Ericmas001.Wpf")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ericmas001@gmail.com")]
 [assembly: AssemblyProduct("Com.Ericmas001.Wpf")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
