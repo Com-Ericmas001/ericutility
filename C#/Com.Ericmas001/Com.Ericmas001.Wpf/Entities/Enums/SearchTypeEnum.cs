69619a600c1d24c3abf0fd35993d233dc139a138
Éric Massé <Ericmas001@users.noreply.github.com>
2014-10-17 11:34:50 +0000

    SearchTypeEnum & SearchTypeAttribute

---
+++ C#/Com.Ericmas001/Com.Ericmas001.Wpf/Entities/Enums/SearchTypeEnum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Com.Ericmas001.Wpf.Entities.Enums
+{
+    public enum SearchTypeEnum
+    {
+        None,
+        Text,
+        UpperText,
+        Int,
+        Date,
+        Time,
+        Guid,
+        List,
+        CheckList
+    }
+}
