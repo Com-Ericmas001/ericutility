7e09256cf5d842676d6e968fe0e0f5240bdf8ae5
ericmas001@gmail.com <ericmas001@gmail.com@7f244a26-2feb-828a-003a-c2b9a63f6a55>
2014-10-17 19:46:20 +0000

    EnumDescriptionConverter

---
+++ trunk/C#/Com.Ericmas001/Com.Ericmas001.Wpf/Converters/EnumDescriptionConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using Com.Ericmas001.Util;
+
+namespace Com.Ericmas001.Wpf.Converters
+{
+    public class EnumDescriptionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            try
+            {
+                return typeof(EnumFactory<>).MakeGenericType(value.GetType()).GetMethod("ToString", new Type[] { value.GetType() }).Invoke(null, new object[] { value }).ToString();
+            }
+            catch
+            {
+                return value.ToString();
+            }
+        }
+
+
+        // No need to implement converting back on a one-way binding 
+        public object ConvertBack(object value, Type targetType,
+          object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ trunk/C#/Com.Ericmas001/WpfApplication1/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    public enum TestEnum
+    {
+        [Description("Non mais Allo quoi !")]
+        Allo,
+        [Description("Bien le Bonjour !")]
+        Bonjour,
+        [Description("Aurevoir !")]
+        Bye
+    }
+    public class MainViewModel
+    {
+        private TestEnum m_TestEnumValue = TestEnum.Bonjour;
+        public TestEnum TestEnumValue { get { return m_TestEnumValue; } }
+    }
+}
