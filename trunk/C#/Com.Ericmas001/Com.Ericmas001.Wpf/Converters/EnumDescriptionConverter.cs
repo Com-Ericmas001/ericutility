b3397eb56abcd5c259cf12dbf777138fc856fcea
ericmas001@gmail.com <ericmas001@gmail.com@7f244a26-2feb-828a-003a-c2b9a63f6a55>
2014-10-18 22:13:16 +0000

    Correction EnumDesciptionConverter

--- trunk/C#/Com.Ericmas001/Com.Ericmas001.Wpf/Converters/EnumDescriptionConverter.cs
+++ trunk/C#/Com.Ericmas001/Com.Ericmas001.Wpf/Converters/EnumDescriptionConverter.cs
@@ -13,6 +13,8 @@ namespace Com.Ericmas001.Wpf.Converters
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
+            if (value == null || parameter == null)
+                return false;
             try
             {
                 return typeof(EnumFactory<>).MakeGenericType(value.GetType()).GetMethod("ToString", new Type[] { value.GetType() }).Invoke(null, new object[] { value }).ToString();
