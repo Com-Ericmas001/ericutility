070ed461c8d101d19fd674a1f8605306ae884196
ericmas001 <ericmas001@7f244a26-2feb-828a-003a-c2b9a63f6a55>
2014-11-22 16:36:51 +0000

    Ajout PrioriteAttribute

---
+++ trunk/C#/Com.Ericmas001/Com.Ericmas001.Util/Attributes/PrioriteAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Com.Ericmas001.Util.Attributes
+{
+    public class PrioriteAttribute
+    {
+        public int Priorite { get; private set; }
+
+        public PrioriteAttribute(int priorite)
+        {
+            Priorite = priorite;
+        }
+    }
+}
