8721d8a44dae414ad968fee7122269a80b4ab206
ericmas001@gmail.com <ericmas001@gmail.com@7f244a26-2feb-828a-003a-c2b9a63f6a55>
2014-12-01 17:36:50 +0000

    Ajout de FormField & FormFieldList

---
+++ trunk/C#/Com.Ericmas001/Com.Ericmas001.Wpf/FormField.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Com.Ericmas001.Util;
+using Com.Ericmas001.Util.Attributes;
+using Com.Ericmas001.Wpf.ViewModels;
+
+namespace Com.Ericmas001.Wpf
+{
+    public class SpecialDefaultValues
+    {
+        public const string DATE_AUJOURDHUI = "##DateTime.Now##";
+    }
+
+    public class FormField<TEnum> : FormField<TEnum, string>
+        where TEnum : struct
+    {
+        public static new FormField<TEnum>[] AllFields
+        {
+            get
+            {
+                return EnumFactory<TEnum>.AllValues.Select(x => new FormField<TEnum>(x)).ToArray();
+            }
+        }
+
+        public FormField(TEnum field)
+            : base(field)
+        {
+
+        }
+    }
+
+    public class FormField<TEnum, TValue> : BaseViewModel
+        where TEnum : struct
+    {
+        public static FormField<TEnum, TValue>[] AllFields
+        {
+            get
+            {
+                return EnumFactory<TEnum>.AllValues.Select(x => new FormField<TEnum, TValue>(x)).ToArray();
+            }
+        }
+
+        private TValue m_Value = default(TValue);
+
+        public TEnum Field { get; private set; }
+        public string Description { get; private set; }
+        public int Priorite { get; private set; }
+
+        public TValue Value
+        {
+            get
+            {
+                return m_Value;
+            }
+            set
+            {
+                m_Value = value;
+                RaisePropertyChanged("Value");
+            }
+        }
+
+        public FormField(TEnum field)
+        {
+            if (!typeof(TEnum).IsEnum)
+                throw new Exception("<TEnum> must be of Enum type (" + typeof(TEnum).Name + ")");
+
+            Field = field;
+            Description = EnumFactory<TEnum>.ToString(field);
+
+            PrioriteAttribute prioAtt = EnumFactory<TEnum>.GetAttribute<PrioriteAttribute>(field);
+            if (prioAtt != null)
+                Priorite = prioAtt.Priorite;
+            else
+                Priorite = 9999;
+
+
+            DefaultValueAttribute defAtt = EnumFactory<TEnum>.GetAttribute<DefaultValueAttribute>(field);
+            if (defAtt != null && typeof(TValue) == typeof(string))
+                Value = (TValue)Convert.ChangeType(GenerateDefaultStringValues(defAtt.Value.ToString()), typeof(string));
+        }
+
+        private string GenerateDefaultStringValues(string def)
+        {
+            switch (def)
+            {
+                case SpecialDefaultValues.DATE_AUJOURDHUI:
+                    return DateTime.Now.ToString();
+            }
+            return def;      
+        }
+    }
+}
