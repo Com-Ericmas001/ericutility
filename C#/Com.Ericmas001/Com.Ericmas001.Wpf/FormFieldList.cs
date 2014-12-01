0116cb3788ab3135b67890f11e9d819d964eeca3
Éric Massé <Ericmas001@users.noreply.github.com>
2014-12-01 17:36:50 +0000

    Ajout de FormField & FormFieldList

---
+++ C#/Com.Ericmas001/Com.Ericmas001.Wpf/FormFieldList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Com.Ericmas001.Wpf
+{
+    public class FormFieldList<TEnum> : FormFieldList<TEnum, string>
+        where TEnum : struct
+    {
+        private FormField<TEnum>[] m_Fields = FormField<TEnum>.AllFields;
+
+        public new FormField<TEnum>[] Fields { get { return m_Fields; } }
+
+        public new FormField<TEnum> GetField(TEnum key)
+        {
+            return m_Fields.First(x => x.Field.Equals(key));
+        }
+
+    }
+    public class FormFieldList<TEnum, TValue>
+        where TEnum : struct
+    {
+        private FormField<TEnum, TValue>[] m_Fields = FormField<TEnum, TValue>.AllFields;
+
+        public FormField<TEnum, TValue>[] Fields { get { return m_Fields; } }
+
+        public FormFieldList()
+        {
+            if (!typeof(TEnum).IsEnum)
+                throw new Exception("<TEnum> must be of Enum type (" + typeof(TEnum).Name + ")");
+        }
+
+        public FormField<TEnum, TValue> GetField(TEnum key)
+        {
+            return m_Fields.First(x => x.Field.Equals(key));
+        }
+
+    }
+}
