59a84f3b9863d2869335edccb6abe56e1729fb61
Éric Massé <Ericmas001@users.noreply.github.com>
2015-01-04 15:06:08 +0000

    Adding Header & Footer Sections (AppMonitorNewSearchView)

--- C#/Com.Ericmas001/Com.Ericmas001.AppMonitor.DataTypes/ViewModels/NewSearchViewModel.cs
+++ C#/Com.Ericmas001/Com.Ericmas001.AppMonitor.DataTypes/ViewModels/NewSearchViewModel.cs
@@ -18,10 +18,28 @@ namespace Com.Ericmas001.AppMonitor.DataTypes.ViewModels
         }
 
         private readonly List<TabSection> m_Sections = new List<TabSection>();
+        private readonly List<ActionButtonSection> m_HeaderActions = new List<ActionButtonSection>();
+        private readonly List<ActionButtonSection> m_FooterActions = new List<ActionButtonSection>();
 
         public TabSection[] Sections
         {
             get { return m_Sections.ToArray(); }
+        }
+        public ActionButtonSection[] HeaderActions
+        {
+            get { return m_HeaderActions.ToArray(); }
+        }
+        public ActionButtonSection[] FooterActions
+        {
+            get { return m_FooterActions.ToArray(); }
+        }
+        public bool HasHeaderActions
+        {
+            get { return m_HeaderActions.Any(); }
+        }
+        public bool HasFooterActions
+        {
+            get { return m_FooterActions.Any(); }
         }
 
         protected virtual IEnumerable<TCategory> ExcludeCategories(IEnumerable<TCategory> categories)
@@ -39,9 +57,27 @@ namespace Com.Ericmas001.AppMonitor.DataTypes.ViewModels
             AddAllCategoriesSection();
         }
 
+        protected virtual void AddAllHeaderActions()
+        {
+        }
+
+        protected virtual void AddAllFooterActions()
+        {
+        }
+
         protected void AddSection(TabSection section)
         {
             m_Sections.Add(CreateSectionWithHandlers(section));
+        }
+
+        protected void AddHeaderAction(ActionButtonSection action)
+        {
+            m_HeaderActions.Add(CreateActionWithHandlers(action));
+        }
+
+        protected void AddFooterAction(ActionButtonSection action)
+        {
+            m_FooterActions.Add(CreateActionWithHandlers(action));
         }
 
         protected virtual TabSection CreateCategorySection(TCategory cat)
@@ -51,13 +87,19 @@ namespace Com.Ericmas001.AppMonitor.DataTypes.ViewModels
 
         public NewSearchViewModel()
         {
-            m_Sections = new List<TabSection>();
-
             AddAllSections();
+            AddAllHeaderActions();
+            AddAllFooterActions();
 
             if (Sections.Any())
                 Sections.First().IsExpanded = true;
 
+        }
+
+        private ActionButtonSection CreateActionWithHandlers(ActionButtonSection action)
+        {
+            action.OnTabCreation += (sender, tab) => CreateNewTab(tab);
+            return action;
         }
 
         private TabSection CreateSectionWithHandlers(TabSection section)
