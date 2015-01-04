5c332c66f343a2d8be7678ff882d3d5a2f7bebeb
ericmas001 <ericmas001@7f244a26-2feb-828a-003a-c2b9a63f6a55>
2015-01-04 15:06:08 +0000

    Adding Header & Footer Sections (AppMonitorNewSearchView)

---
+++ trunk/C#/Com.Ericmas001/Com.Ericmas001.AppMonitor.DataTypes/ViewModels/Sections/ActionButtonSection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+using Com.Ericmas001.AppMonitor.DataTypes.Attributes;
+using Com.Ericmas001.AppMonitor.DataTypes.Entities;
+using Com.Ericmas001.AppMonitor.DataTypes.Helpers;
+using Com.Ericmas001.Wpf;
+using Com.Ericmas001.Wpf.Entities.Enums;
+using Com.Ericmas001.Wpf.ViewModels.SearchElements;
+
+namespace Com.Ericmas001.AppMonitor.DataTypes.ViewModels.Sections
+{
+    public abstract class ActionButtonSection : TabSection
+    {
+        private RelayCommand m_ExecuteCommand;
+        public ICommand ExecuteCommand
+        {
+            get { return m_ExecuteCommand ?? (m_ExecuteCommand = new RelayCommand(x => CreateContentTab())); }
+        }
+    }
+}
