2c370bd59d2b30f5c13751197e7628e4b1742fa9
Éric Massé <Ericmas001@users.noreply.github.com>
2015-01-05 20:02:09 +0000

    Adding A Query Layer

---
+++ C#/Com.Ericmas001/Com.Ericmas001.AppMonitor.DataTypes/ViewModels/BaseQueryResultViewModel.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Threading;
+using Com.Ericmas001.AppMonitor.DataTypes.Attributes;
+using Com.Ericmas001.AppMonitor.DataTypes.Entities;
+using Com.Ericmas001.AppMonitor.DataTypes.Enums;
+using Com.Ericmas001.AppMonitor.DataTypes.Helpers;
+using Com.Ericmas001.AppMonitor.DataTypes.TreeElements;
+using Com.Ericmas001.AppMonitor.DataTypes.ViewModels.Sections;
+using Com.Ericmas001.Util;
+using Com.Ericmas001.Wpf.ViewModels.Trees;
+
+namespace Com.Ericmas001.AppMonitor.DataTypes.ViewModels
+{
+    public abstract class BaseQueryResultViewModel<TDataItem> : BaseGroupedContentViewModel
+        where TDataItem : IDataItem
+    {
+        private readonly string m_Keyword;
+        private readonly string m_SearchCriteria;
+        protected BunchOfDataItems<TDataItem> m_DataItems = new BunchOfDataItems<TDataItem>();
+
+        public override string TabHeader
+        {
+            get { return string.Format("{0} {1}", Keyword); }
+        }
+
+        public override bool CanCloseTab
+        {
+            get { return true; }
+        }
+
+        protected IEnumerable<TDataItem> Data
+        {
+            get { return m_DataItems.Data; }
+        }
+
+        protected string Keyword
+        {
+            get { return m_Keyword; }
+        }
+
+        protected string SearchCriteria
+        {
+            get { return m_SearchCriteria; }
+        }
+
+        public BaseQueryResultViewModel(Dispatcher appCurrentDispatcher, string keyword, string criteria)
+            : base(appCurrentDispatcher)
+        {
+            m_Keyword = keyword;
+            m_SearchCriteria = criteria;
+
+            InitGroupingAndFiltering();
+            ChooseGroupVm.OnGroupsChanged += delegate { RefreshInterface(); };
+
+            RefreshDataAndInterface();
+        }
+
+        protected virtual void InitGroupingAndFiltering()
+        {
+            ChooseGroupVm = new ChooseGroupViewModel(m_DataItems, GetAllGroupingCriterias(), OrderCriterias, new string[0]);
+        }
+
+        public abstract string[] GetAllGroupingCriterias();
+
+        protected virtual IEnumerable<string> OrderCriterias(IEnumerable<string> criterias)
+        {
+            return criterias.OrderBy(x => x);
+        }
+        protected virtual IEnumerable<TDataItem> OrderItems(IEnumerable<TDataItem> items, IEnumerable<string> allCriterias)
+        {
+            return items;
+        }
+        protected virtual string ObtainOrdering(IEnumerable<string> usedCriterias, string criteria, IGrouping<String, TDataItem> group)
+        {
+            return group.Key;
+        }
+
+        protected virtual string ObtainGroupingValue(TDataItem item, string criteria)
+        {
+            return item.ObtainValue(criteria);
+        }
+
+        public virtual IGrouping<string, TDataItem>[] ObtainGrouping(string criteria, IEnumerable<TDataItem> items)
+        {
+            return items.GroupBy(x => ObtainGroupingValue(x,criteria)).ToArray();
+        }
+
+        protected abstract BaseLeafTreeElement CreateLeaf(TreeElementViewModel parent, TDataItem item, IEnumerable<string> criteres);
+        protected abstract BaseBranchTreeElement CreateBranch(TreeElementViewModel parent, string currentCritere, string value, IEnumerable<string> usedCriteres);
+
+
+        protected virtual string[] GetAllFiltersCriteria()
+        {
+            return GetAllGroupingCriterias();
+        }
+
+        public virtual IEnumerable<FilterEnum> GenerateFilter(string crit)
+        {
+            return new FilterEnum[] { FilterEnum.Text };
+        }
+
+        public virtual IEnumerable<string> GroupedCriterias()
+        {
+            return ChooseGroupVm.ChoosenGroups.Items;
+        }
+
+        protected List<TreeElementViewModel> FillTree(TreeElementViewModel node, IEnumerable<TDataItem> items, IEnumerable<string> criterias, IEnumerable<string> allCriterias)
+        {
+            var result = new List<TreeElementViewModel>();
+            var allCriteriasArray = allCriterias as string[] ?? allCriterias.ToArray();
+            var criteriasArray = criterias as string[] ?? criterias.ToArray();
+
+            if ((criterias == null | !criteriasArray.Any()))
+            {
+                foreach (TDataItem elem in OrderItems(items, allCriteriasArray))
+                {
+                    BaseLeafTreeElement leaf = CreateLeaf(node, elem, allCriteriasArray);
+                    if (leaf != null)
+                    {
+                        leaf.OnTabCreation += HandlingTabCreation;
+                        result.Add(leaf);
+                    }
+                }
+            }
+            else
+            {
+                string currentCriteria = criteriasArray.First();
+                string[] usedCriterias = allCriteriasArray.Except(criteriasArray).Union(new[] {currentCriteria}).ToArray();
+
+                IGrouping<string, TDataItem>[] groups = ObtainGrouping(currentCriteria, items);
+                if (groups != null)
+                {
+                    foreach (IGrouping<String, TDataItem> x in groups.OrderBy(g => ObtainOrdering(usedCriterias, currentCriteria, g)))
+                    {
+                        BaseBranchTreeElement nextnode = CreateBranch(node, currentCriteria, x.Key, usedCriterias);
+                        nextnode.Children.AddItems(FillTree(nextnode, x, criteriasArray.Skip(1), allCriteriasArray));
+                        nextnode.OnTabCreation += HandlingTabCreation;
+                        result.Add(nextnode);
+                    }
+                }
+            }
+            return result;
+        }
+
+        private void GenerateFilters()
+        {
+            var filters = new Dictionary<string, FilterEnum[]>();
+            foreach (string crit in GetAllFiltersCriteria())
+            {
+                IEnumerable<FilterEnum> myfilters = GenerateFilter(crit);
+                if (myfilters != null && myfilters.Any())
+                    filters.Add(crit, myfilters.ToArray());
+            }
+            ChooseGroupVm.FieldsToFilter = filters;
+        }
+
+        private IEnumerable<TDataItem> GetFilteredData()
+        {
+
+            TDataItem[] filteredData = Data.ToArray();
+            foreach (Filter f in ChooseGroupVm.CurrentFilters)
+            {
+                string crit = f.Field;
+                filteredData = filteredData.Where(d => f.IsSurvivingTheFilter(d.ObtainFilterValue(crit))).ToArray();
+            }
+
+            return filteredData;
+        }
+
+        protected override void BuildTree(object sender, DoWorkEventArgs e)
+        {
+            string[] criteres = GroupedCriterias().ToArray();
+            GenerateFilters();
+            CachedTreeElements = FillTree(null, GetFilteredData(), criteres, criteres);
+        }
+    }
+}
--- C#/Com.Ericmas001/Com.Ericmas001.AppMonitor.DataTypes/ViewModels/BaseSearchResultViewModel.cs
+++ C#/Com.Ericmas001/Com.Ericmas001.AppMonitor.DataTypes/ViewModels/BaseSearchResultViewModel.cs
@@ -14,7 +14,7 @@ using Com.Ericmas001.Wpf.ViewModels.Trees;
 
 namespace Com.Ericmas001.AppMonitor.DataTypes.ViewModels
 {
-    public abstract class BaseSearchResultViewModel<TCategory, TCriteria, TDataItem, TGroupingAttribute> : BaseGroupedContentViewModel
+    public abstract class BaseSearchResultViewModel<TCategory, TCriteria, TDataItem, TGroupingAttribute> : BaseQueryResultViewModel<TDataItem>
         where TCategory : struct
         where TCriteria : struct
         where TDataItem : IDataItem<TCriteria>
@@ -24,10 +24,7 @@ namespace Com.Ericmas001.AppMonitor.DataTypes.ViewModels
 
         private readonly TCriteria m_SearchCriteria;
 
-        private readonly string m_Keyword;
-
         protected abstract TCategory Category { get; }
-        protected BunchOfDataItems<TDataItem> m_DataItems = new BunchOfDataItems<TDataItem>();
 
         protected override string IconBigImageName
         {
@@ -57,118 +54,59 @@ namespace Com.Ericmas001.AppMonitor.DataTypes.ViewModels
             get { return string.Format("{0} {1}", CriteriaHelper<TCriteria, TCategory>.GenerateHeaderTag(SearchCriteria), Keyword); }
         }
 
-        public override bool CanCloseTab
+        protected new TCriteria SearchCriteria
         {
-            get { return true; }
+            get { return EnumFactory<TCriteria>.Parse(base.SearchCriteria); }
         }
 
-        protected IEnumerable<TDataItem> Data
+        public BaseSearchResultViewModel(Dispatcher appCurrentDispatcher, TCriteria criteria, string keyword)
+            : base(appCurrentDispatcher, keyword, EnumFactory<TCriteria>.ToString(criteria))
         {
-            get { return m_DataItems.Data; }
         }
 
-        protected TCriteria SearchCriteria
+        public override string[] GetAllGroupingCriterias()
         {
-            get { return m_SearchCriteria; }
-        }
-
-        protected string Keyword
-        {
-            get { return m_Keyword; }
-        }
-
-        public BaseSearchResultViewModel(Dispatcher appCurrentDispatcher, TCriteria criteria, string keyword) : base(appCurrentDispatcher)
-        {
-            m_SearchCriteria = criteria;
-            m_Keyword = keyword;
-
-            InitGroupingAndFiltering();
-            ChooseGroupVm.OnGroupsChanged += delegate { RefreshInterface(); };
-
-            RefreshDataAndInterface();
-        }
-
-        protected virtual void InitGroupingAndFiltering()
-        {
-            TCriteria[] allAvailables = CriteriaHelper<TCriteria, TCategory>.GetAllGroupingCriterias<TGroupingAttribute>(Category).Where(crit => !crit.Equals(SearchCriteria)).ToArray();
-            IEnumerable<string> availables = allAvailables.Select(EnumFactory<TCriteria>.ToString);
-            IEnumerable<string> alreadyGrouped = new String[0];
-            ChooseGroupVm = new ChooseGroupViewModel(m_DataItems, availables,CriteriaHelper<TCriteria, TCategory>.OrderCriteriaStrings, alreadyGrouped);
+            return CriteriaHelper<TCriteria, TCategory>.GetAllGroupingCriterias<TGroupingAttribute>(Category).Where(crit => !crit.Equals(SearchCriteria)).Select(x=>EnumFactory<TCriteria>.ToString(x)).ToArray();
         }
 
         protected abstract BaseLeafTreeElement CreateLeaf(TreeElementViewModel parent, TDataItem item, IEnumerable<TCriteria> criteres);
+        protected override BaseLeafTreeElement CreateLeaf(TreeElementViewModel parent, TDataItem item, IEnumerable<string> criteres)
+        {
+            return CreateLeaf(parent, item, criteres.Select(x => EnumFactory<TCriteria>.Parse(x)));
+        }
+
         protected abstract BaseBranchTreeElement CreateBranch(TreeElementViewModel parent, TCriteria currentCritere, string value, IEnumerable<TCriteria> usedCriteres, TCategory category);
-
-        protected List<TreeElementViewModel> FillTree(TreeElementViewModel node, IEnumerable<TDataItem> items, IEnumerable<TCriteria> criterias, IEnumerable<TCriteria> allCriterias)
+        protected override BaseBranchTreeElement CreateBranch(TreeElementViewModel parent, string currentCritere, string value, IEnumerable<string> usedCriteres)
         {
-            var result = new List<TreeElementViewModel>();
-            var allCriteriasArray = allCriterias as TCriteria[] ?? allCriterias.ToArray();
-            var criteriasArray = criterias as TCriteria[] ?? criterias.ToArray();
-
-            if ((criterias == null | !criteriasArray.Any()))
-            {
-                foreach (TDataItem elem in items.OrderBy(x => x.ObtainDateTime(allCriteriasArray)))
-                {
-                    BaseLeafTreeElement leaf = CreateLeaf(node, elem, allCriteriasArray);
-                    if (leaf != null)
-                    {
-                        leaf.OnTabCreation += HandlingTabCreation;
-                        result.Add(leaf);
-                    }
-                }
-            }
-            else
-            {
-                TCriteria currentCriteria = criteriasArray.First();
-                TCriteria[] usedCriterias = allCriteriasArray.Except(criteriasArray).Union(new[] {currentCriteria}).ToArray();
-
-                IGrouping<string, TDataItem>[] groups = CriteriaHelper<TCriteria, TCategory>.ObtainGrouping(currentCriteria, items);
-                if (groups != null)
-                {
-                    foreach (IGrouping<String, TDataItem> x in groups.OrderBy(g => CriteriaHelper<TCriteria, TCategory>.ObtainOrdering(usedCriterias, currentCriteria, g)))
-                    {
-                        BaseBranchTreeElement nextnode = CreateBranch(node, currentCriteria, x.Key, usedCriterias, Category);
-                        nextnode.Children.AddItems(FillTree(nextnode, x, criteriasArray.Skip(1), allCriteriasArray));
-                        nextnode.OnTabCreation += HandlingTabCreation;
-                        result.Add(nextnode);
-                    }
-                }
-            }
-            return result;
+            return CreateBranch(parent, EnumFactory<TCriteria>.Parse(currentCritere), value, usedCriteres.Select(x => EnumFactory<TCriteria>.Parse(x)), Category);
         }
 
-        private void GenerateFilters()
+        protected override IEnumerable<string> OrderCriterias(IEnumerable<string> criterias)
         {
-            var filters = new Dictionary<string, FilterEnum[]>();
-            foreach (TCriteria crit in CriteriaHelper<TCriteria, TCategory>.GetAllGroupingCriterias<TGroupingAttribute>(Category).Where(crit => !crit.Equals(SearchCriteria)))
-            {
-                var filterAtt = EnumFactory<TCriteria>.GetAttribute<FiltersAttribute>(crit);
-                if ((filterAtt != null && !filterAtt.Filters.Contains(FilterEnum.None)))
-                    filters.Add(EnumFactory<TCriteria>.ToString(crit), filterAtt.Filters.ToArray());
-            }
-            ChooseGroupVm.FieldsToFilter = filters;
+            return CriteriaHelper<TCriteria, TCategory>.OrderCriteriaStrings(criterias);
         }
-
-        private IEnumerable<TDataItem> GetFilteredData()
+        protected override IEnumerable<TDataItem> OrderItems(IEnumerable<TDataItem> items, IEnumerable<string> allCriterias)
         {
-
-            TDataItem[] filteredData = Data.ToArray();
-            foreach (Filter f in ChooseGroupVm.CurrentFilters)
-            {
-                TCriteria crit = EnumFactory<TCriteria>.Parse(f.Field);
-                filteredData = filteredData.Where(d => f.IsSurvivingTheFilter(d.ObtainFilterValue(crit))).ToArray();
-            }
-
-            return filteredData;
+            return items.OrderBy(x => x.ObtainDateTime(allCriterias.Select(y => EnumFactory<TCriteria>.Parse(y))));
         }
-
-        protected override void BuildTree(object sender, DoWorkEventArgs e)
+        protected override string ObtainOrdering(IEnumerable<string> usedCriterias, string criteria, IGrouping<string, TDataItem> group)
         {
-            TCriteria[] criteres = new[] { SearchCriteria }.Union(ChooseGroupVm.ChoosenGroups.Items.Select(EnumFactory<TCriteria>.Parse)).ToArray();
-
-            GenerateFilters();
-
-            CachedTreeElements = FillTree(null, GetFilteredData(), criteres, criteres);
+            return CriteriaHelper<TCriteria, TCategory>.ObtainOrdering(usedCriterias.Select(x => EnumFactory<TCriteria>.Parse(x)), EnumFactory<TCriteria>.Parse(criteria), group);
+        }
+        public override IGrouping<string, TDataItem>[] ObtainGrouping(string criteria, IEnumerable<TDataItem> items)
+        {
+            return CriteriaHelper<TCriteria, TCategory>.ObtainGrouping(EnumFactory<TCriteria>.Parse(criteria), items);
+        }
+        public override IEnumerable<FilterEnum> GenerateFilter(string crit)
+        {
+            var filterAtt = EnumFactory<TCriteria>.GetAttribute<FiltersAttribute>(EnumFactory<TCriteria>.Parse(crit));
+            if ((filterAtt != null && !filterAtt.Filters.Contains(FilterEnum.None)))
+                return filterAtt.Filters.ToArray();
+            return null;
+        }
+        public override IEnumerable<string> GroupedCriterias()
+        {
+            return new[] { EnumFactory<TCriteria>.ToString(SearchCriteria)}.Union(base.GroupedCriterias());
         }
     }
 }
