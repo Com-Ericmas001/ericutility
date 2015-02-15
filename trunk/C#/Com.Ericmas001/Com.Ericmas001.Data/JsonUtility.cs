644042d474646f113761d6827a162b3d70d55ff9
ericmas001 <ericmas001@7f244a26-2feb-828a-003a-c2b9a63f6a55>
2015-02-15 02:40:10 +0000

    That was not that cool...

--- trunk/C#/Com.Ericmas001/Com.Ericmas001.Data/JsonUtility.cs
+++ trunk/C#/Com.Ericmas001/Com.Ericmas001.Data/JsonUtility.cs
@@ -8,13 +8,13 @@ namespace Com.Ericmas001.Data
 {
     public static class JsonUtility
     {
-        public static string SerializeObject(object obj, bool indented = true, bool classNames = false)
-        {
-            return JsonConvert.SerializeObject(obj, indented ? Formatting.Indented : Formatting.None, new JsonSerializerSettings { TypeNameHandling = classNames ? TypeNameHandling.All : TypeNameHandling.None });
-        }
-        public static T DeserializeObject<T>(string serialized, bool classNames = false)
-        {
-            return JsonConvert.DeserializeObject<T>(serialized, new JsonSerializerSettings {TypeNameHandling = classNames ? TypeNameHandling.All : TypeNameHandling.None});
-        }
+        //public static string SerializeObject(object obj, bool indented = true, bool classNames = false)
+        //{
+        //    return JsonConvert.SerializeObject(obj, indented ? Formatting.Indented : Formatting.None, new JsonSerializerSettings { TypeNameHandling = classNames ? TypeNameHandling.All : TypeNameHandling.None });
+        //}
+        //public static T DeserializeObject<T>(string serialized, bool classNames = false)
+        //{
+        //    return JsonConvert.DeserializeObject<T>(serialized, new JsonSerializerSettings {TypeNameHandling = classNames ? TypeNameHandling.All : TypeNameHandling.None});
+        //}
     }
 }
