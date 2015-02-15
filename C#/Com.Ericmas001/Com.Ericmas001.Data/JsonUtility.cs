2a1e31783aae7add9b67d5eda8a961308ce16754
Éric Massé <Ericmas001@users.noreply.github.com>
2015-02-15 02:31:26 +0000

    Adding JsonUtility

---
+++ C#/Com.Ericmas001/Com.Ericmas001.Data/JsonUtility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Com.Ericmas001.Data
+{
+    public static class JsonUtility
+    {
+        public static string SerializeObject(object obj, bool indented = true, bool classNames = false)
+        {
+            return JsonConvert.SerializeObject(obj, indented ? Formatting.Indented : Formatting.None, new JsonSerializerSettings { TypeNameHandling = classNames ? TypeNameHandling.All : TypeNameHandling.None });
+        }
+        public static T DeserializeObject<T>(string serialized, bool classNames = false)
+        {
+            return JsonConvert.DeserializeObject<T>(serialized, new JsonSerializerSettings {TypeNameHandling = classNames ? TypeNameHandling.All : TypeNameHandling.None});
+        }
+    }
+}
