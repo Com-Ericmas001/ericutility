ff587fbe543ac9ec67228a916b114d7af71bcd39
Eric Masse <ericmas001@hotmail.com>
2015-04-19 13:04:02 -0400

    Cleaning a little

---
+++ C#/Com.Ericmas001/Com.Ericmas001.Net.Protocol/AbstractCustomJsonConverter.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace Com.Ericmas001.Net.Protocol
+{
+    public abstract class AbstractCustomJsonConverter<T> : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return (objectType == typeof(T));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var jObject = JObject.Load(reader);
+            var instance = ObtainCustomObject(jObject);
+            if (instance != null)
+                serializer.Populate(jObject.CreateReader(), instance);
+            return instance;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            serializer.Serialize(writer, value);
+        }
+
+        public abstract T ObtainCustomObject(JObject jObject);
+    }
+}
---
+++ C#/Com.Ericmas001/Com.Ericmas001.Net.Protocol/OptionJsonConverter.cs
@@ -0,0 +1,14 @@
+﻿using Com.Ericmas001.Util.Options;
+using Newtonsoft.Json.Linq;
+
+namespace Com.Ericmas001.Net.Protocol
+{
+    public class OptionJsonConverter<TOption, TEnum> : AbstractCustomJsonConverter<TOption>
+        where TOption : IOption<TEnum>
+    {
+        public override TOption ObtainCustomObject(JObject jObject)
+        {
+            return FactoryOption<TOption, TEnum>.GenerateOption(jObject.Value<string>("OptionType"));
+        }
+    }
+}
