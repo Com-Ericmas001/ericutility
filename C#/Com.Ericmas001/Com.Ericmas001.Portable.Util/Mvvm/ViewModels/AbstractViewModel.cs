5fa8d41cf6da160d1ac6f0fb56a6a216667ba372
Éric Massé <Ericmas001@gmail.com>
2015-09-23 20:49:33 -0400

    Oops

---
+++ C#/Com.Ericmas001/Com.Ericmas001.Portable.Util/Mvvm/ViewModels/AbstractViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Com.Ericmas001.Portable.Util.Mvvm.ViewModels
+{
+    public class AbstractViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+        public event EventHandler OnRequestClose = delegate { };
+
+        public virtual string Error
+        {
+            get { return null; }
+        }
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void CloseView()
+        {
+            OnRequestClose(this, new EventArgs());
+        }
+    }
+}
